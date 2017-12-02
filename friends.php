b4e0c160f47197e898dab195dbc9c822f290a53a
Daniel-Florin Dosaru <dosarudaniel@yahoo.com>
2017-12-02 16:45:20 +0200

    Add files via upload

---
+++ friends.php
@@ -0,0 +1,18 @@
+[{"id":"71","title":"Navratri 2017: 3 ways to wear purple on Navami (Day 9)","category":"Life & Style ","date":"2017-09-08","image":"http:\/\/mobileappdatabase.in\/demo\/smartnews\/app_dashboard\/uploads\/98172339_Image.jpg","description":"
+Maa Siddhidatri the ninth form of Goddess Parvati, is worshipped on the ninth day of Navaratri. She is considered to have healing powers. Always seen in a happy pose, the goddess represents knowledge and wisdom. She has four arms and is seen seated on a lotus flower.<\/p>\r\n\r\n
+
+
+\r\nThe colour purple symbolises the stabilty of blue and the strength of red. It signifies wealth, power and wisdom. Hence, this colour is usually worn on Navami. To make your look festive, you can pick a traditional sari and give it a modern twist. You can also opt for more comfortable clothes like an anarkali or lehenga, if dandiya is on your mind.<\/p>\r\n\r\n
+
+
+\r\n1) Purple is one of the brightest colours and the strategy to wear it right is to mix it with other brighter colours just like Bollywood diva Shilpa Shetty has done here. The gorgeous actor wore a purple sari with golden stripes from Raw Mango. To make her look more interesting, she contrasted the simple sari with a bright pink colour blouse and teamed it with silver danglers from Amrapali jewellers. Smokey eyes and red lips rounded out her look.<\/p>\r\n\r\n
+
+\"\"<\/p>\r\n\r\n
+
+2) Kriti Sanon is seen wearing a Manish Malhotra purple colour floor-length anarkali. The silver embroidery helped break the monotony of the outfit. Since the texture is already shiny, she went for a no accessories and minimal make-up look. We think it’s the best approach.<\/p>\r\n\r\n
+
+\"\"<\/p>\r\n\r\n
+
+3) Shararas might not be the usual choice of outfit for Navratri celebrations, but variety is the spice of life and if you have never tried wearing one, then, this might be the right time to do so. Gauahar Khan recently made an appearance in a bright purple Mayyur Girotra sharara from their Ganga winter\/festive’17 collection. Styled by celebrity stylist Devki B, she looked festive ready in the banarasi and zardozi outfit. With minimal make-up, red lips, hair tied in a neat bun, she completed her look with earrings from Purab Paschim by Ankit Khullar.<\/p>\r\n\r\n
+
+\"\"<\/p>\r\n"}]
